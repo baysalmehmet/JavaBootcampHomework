069585a1de5072a4faaef5273d4c1b6efb6c107e
Mehmet Baysal <71693746+baysalmehmet@users.noreply.github.com>
2022-10-30 15:03:26 +0300

    Delete week4/todo directory

--- week4/todo/src/main/java/com/Homework/todo/repository/TodoRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Homework.todo.repository;
-
-import com.Homework.todo.entities.TodoList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepo extends JpaRepository<TodoList, Long> {
-}
